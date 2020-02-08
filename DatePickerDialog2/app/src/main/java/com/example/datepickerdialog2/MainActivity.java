7480906ab7bff933c621098aa9f0ba1cd5df240a
Ayberk Gerey <ayberkgerey@gmail.com>
2020-02-08 21:37:16 +0300

    Add files via upload

---
+++ DatePickerDialog2/app/src/main/java/com/example/datepickerdialog2/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.datepickerdialog2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements  DatePickerDialog.OnDateSetListener {
+
+    private TextView tvDate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvDate = (TextView) findViewById(R.id.tvDate);
+        findViewById(R.id.btnShowDate).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
+    }
+
+    private void showDatePickerDialog(){
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+             datePickerDialog.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        String date = dayOfMonth + "/" + month + "/" + year;
+        tvDate.setText(date);
+
+    }
+}
