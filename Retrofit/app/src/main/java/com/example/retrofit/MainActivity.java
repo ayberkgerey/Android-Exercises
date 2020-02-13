926ca54fa5e4b222cdc9430a558ccd6d9a0369bc
Ayberk Gerey <ayberkgerey@gmail.com>
2020-02-13 14:38:58 +0300

    Add files via upload

--- Retrofit/app/src/main/java/com/example/retrofit/MainActivity.java
+++ Retrofit/app/src/main/java/com/example/retrofit/MainActivity.java
@@ -16,6 +16,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewResult;
+    private JsonPlaceHolderApi jsonPlaceHolderApi;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +25,60 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textViewResult = findViewById(R.id.text_view_result);
+        RetrofitInitialize();
+
+        getComments();
+
+    }
+
+
+    private void RetrofitInitialize(){
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://jsonplaceholder.typicode.com/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
 
+
+    }
+
+
+    private void getComments() {
+
+        Call<List<Comment>> call = jsonPlaceHolderApi.getComments(3);
+
+        call.enqueue(new Callback<List<Comment>>() {
+            @Override
+            public void onResponse(Call<List<Comment>> call, Response<List<Comment>> response) {
+                if(!response.isSuccessful()){
+                    textViewResult.setText("Code : " + response.code());
+                    return;
+                }
+                List<Comment> comments = response.body();
+                for (Comment comment : comments){
+                    String content = " ";
+                    content += "PostId : " + comment.getPostId() + "\n";
+                    content += "ID : " + comment.getId() + "\n";
+                    content += "Name : " + comment.getName() + "\n";
+                    content += "Email : " + comment.getEmail() + "\n";
+                    content += "Text : " + comment.getText() + "\n\n";
+
+                    textViewResult.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Comment>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
+
+
+    private void getPosts() {
         Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
 
 
@@ -61,4 +109,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+
 }
